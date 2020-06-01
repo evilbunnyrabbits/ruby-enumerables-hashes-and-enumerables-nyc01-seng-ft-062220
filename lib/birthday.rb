997e042b0eb02c05fe5a5e00ce1c3d20b88eb757
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-06-01 14:43:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,6 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.
-  
-  
-  return "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
